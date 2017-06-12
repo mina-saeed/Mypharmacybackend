679e57816f39a5dcafad9998979c0101be377bd1
mahmoud <mahmoud@beeieg.com>
2017-06-13 01:30:19 +0200

    User Login With Facebook and save data on mongoDB

---
+++ index.js
@@ -0,0 +1,90 @@
+const config = require('./config.json')
+var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+var bodyParser = require("body-parser");
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/pharmacy";
+
+var app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+
+
+passport.use(new Strategy({
+        clientID: config.AppId,
+        clientSecret: config.AppSecret,
+        callbackURL: config.callback,
+        enableProof: true,
+        profileFields: ['id', 'displayName', 'link','gender',  'photos', 'emails']
+        },
+        function(accessToken, refreshToken, profile, cb) {
+                var email = "";
+                var user ="";
+                        if(profile.emails){
+                                email = profile.emails[0].value
+                        }
+
+                MongoClient.connect(url, function(err, db) {
+
+                        if (err) throw err;
+                                var user = { 
+                                        fb_id: profile.id,
+                                        email: email,
+                                        username: profile.displayName,
+                                        gender: profile.gender,
+                                        link: profile.link,
+                                        access_token: accessToken
+
+                                 };
+                                 db.collection('users').find({fb_id: profile.id}).toArray(function(err, result){
+                                                if (err)
+                                                        return cb(err);
+                                                if(result){
+                                                        return cb(null, user); // user found, return that user
+                                                }
+                                                else{
+                                                db.collection("users").insertOne(user, function(err, res) {
+                                                        if (err){
+                                                        }else{
+                                                              //  console.log(res)
+                                                                console.log("1 record inserted");
+                                                                
+                                                        }
+                                
+                                                        db.close();
+                                                        return cb(null, user);
+                                                });
+                                        }
+
+
+                                })
+
+                });
+        }));
+
+app.get('/login/facebook', passport.authenticate('facebook' , {scope:'email'}));
+app.get('/login', passport.authenticate('facebook' , {scope:'email'}));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect : '/',failureRedirect: '/login' }),function(req, res) {console.log("res") });
+
+app.get('/', function(req,res){
+res.redirect('/')
+})
+
+
+app.listen(3000, function() {
+    console.log("Listening!")
+})
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+})
