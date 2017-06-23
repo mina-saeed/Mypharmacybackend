facd611151b670930f0f03d201f2712a44e9c70f
Mahmoud <mahmoudsror@gmail.com>
2017-06-23 04:24:36 +0000

    New order and save image  also integrate user endpoints with ionic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const config = require('./config.json')
 var express = require('express');
 var passport = require('passport');
+const fs = require('fs');
+const http = require('http')
 var Strategy = require('passport-facebook').Strategy;
 var bodyParser = require("body-parser");
 var MongoClient = require('mongodb').MongoClient;
@@ -38,7 +40,6 @@ passport.use(new Strategy({
                         }
 
                 MongoClient.connect(url, function(err, db) {
-
                         if (err) throw err;
                                 var user = { 
                                         fb_id: profile.id,
@@ -52,14 +53,13 @@ passport.use(new Strategy({
                                  db.collection('users').find({fb_id: profile.id}).toArray(function(err, result){
                                                 if (err)
                                                         return cb(err);
-                                                if(result){
+                                                if(result.length>0){
                                                         return cb(null, user); // user found, return that user
                                                 }
                                                 else{
                                                 db.collection("users").insertOne(user, function(err, res) {
                                                         if (err){
                                                         }else{
-                                                              //  console.log(res)
                                                                 console.log("1 record inserted");
                                                                 
                                                         }
@@ -77,57 +77,201 @@ passport.use(new Strategy({
 
 app.get('/login/facebook', passport.authenticate('facebook' , {scope:'email'}));
 app.get('/login', passport.authenticate('facebook' , {scope:'email'}));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect : '/userHome',failureRedirect: '/login' }),function(req, res) {console.log("res") });
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect : '/userHome',failureRedirect: '/login' }),function(req, res) { });
 
-app.get('/userHome', function(req,res){
-res.redirect('/userHome')
+app.get('/userHome/:deviceID', function(req,res){
+
+    var user_device_id = req.params.deviceID
+    console.log(user_device_id)
+        MongoClient.connect(url, function(err, db) {
+
+            db.collection('users').find({deviceID: user_device_id , mobile:{$ne:null}, location:{$ne:null} }).toArray(function(err, result){
+                    
+                    if(err){
+                            res.send("Error")
+                    }
+                    if(result.length >0){
+                            
+                        //res.redirect('/order')
+                        res.status(200).send(result)
+                    }
+            })
+    });
+
 })
 
+app.post('/user/submit',staticUserAuth, function(req,res){
+    console.log(req.body)
+    var user_mobile=req.body.mobile
+    var user_telephone=req.body.telephone
+    var user_device_id=req.body.deviceID
+    var user_location = req.body.location
 
+        MongoClient.connect(url, function(err, db) {
+        db.collection('users').update(
+                {deviceID: user_device_id},
+                {$set:
+                        {
+                            mobile: user_mobile,
+                            telephone: user_telephone,
+                            deviceID: user_device_id,
+                            location:  user_location 
+                        },
 
-/******************************* Start Of Guest Login Endpoint ****************************************************/
-app.post('/guest/login', staticUserAuth, function(req, res) {
-   // console.log(req)
-    var guest_name=req.body.name
-    var guest_mobile=req.body.mobile
-    var guest_address=req.body.address
-    var guest_device_id=req.body.deviceID
-MongoClient.connect(url, function(err, db) {    
-    db.collection('guest').find({mobile: guest_mobile}).toArray(function(err, result){
+                        
+                },
+                {
+                    upsert:true
+                },
+                function(err, result){
 
-        if(err){
-            res.send("Error")
-        }
-        if(result){
-            //console.log(result)
-            res.redirect('/userHome')
-        }
-        if(result==null){
-            var guest={
-                name: guest_name,
-                mobile: guest_mobile,
-                address: guest_address,
-                deviceID: guest_device_id
+                        if(err) {throw err}
+                        else{
 
-            }
-            db.collection('guest').insertOne(guest , function(err, output){
-
-                if(err){
-                    console.log("error")
-                }else{
-
-                    console.log("Guest has been added")
-                    res.redirect('/userHome')
+                            db.collection('users').find({deviceID: user_device_id }).toArray(function(err, result){
+                    
+                                if(err){
+                                    res.send("Error")
+                                }
+                                if(result.length >0){
+                            
+                                    res.status(200).send(result)
+                                }
+                            })
+                        }        
                 }
-            })
-        }
-    })
+        )
                         
    });   
+
 })
-/*******************************************End Of Guest Login Endpoint ***************************/
+app.post('/user/editLocation',staticUserAuth, function(req,res){
+
+    var user_device_id=req.body.deviceID
+    var user_location = req.body.location
+
+    MongoClient.connect(url, function(err, db) {
+        db.collection('users').update(
+                {deviceID: user_device_id},
+                {$set:
+                        {
+                            location:  user_location 
+                        },       
+                },
+                function(err, result){
+
+                        if(err) {throw err}
+                        else{
+
+                            res.status(200)
+                        }        
+                }
+        )
+                        
+   });   
+
+})
+/*app.post('/order/uploadImage',staticUserAuth, function(req,res){
+
+        var user_device_id=req.body.deviceID
+        var user_image = (req.body.image).toLowerCase()
+
+        var image_dir = __dirname + 'orderImages/'
+        var image_url = __dirname + 'orderImages/'+user_image
+
+        fs.rename(image_url, user_image, function(err) {
+            if (err) {
+                console.log(err);
+                res.send(403);
+            } else {
+                MongoClient.connect(url, function(err, db) {
+                        if (err) throw err;
+                                var order = { 
+                                        userID: user_device_id,
+                                        image_url: image_url
+
+                                 };
 
 
+                                                db.collection("orders").insertOne(order, function(err, res) {
+                                                        if (err){
+                                                        }else{
+                                                                console.log("1 order inserted");
+                                                                
+                                                        }
+                                
+                                                        db.close();
+                                                });
+                                        
+
+
+
+                });
+            }
+        });
+
+
+
+})*/
+app.post('/order/submit',staticUserAuth, function(req,res){
+var fullUrl = "http://207.154.240.16/orderImages/";
+    var order_user=req.body.userID
+    var order_pharmacy=req.body.pharmacy
+    var order_products=req.body.products
+    var order_price = req.body.price
+    var order_location = req.body.location
+    var order_time = req.body.time
+    var order_status = req.body.status
+    var order_image = "test.jpg"
+    var image_url = fullUrl+order_image
+
+        MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+                var order = { 
+                        userID: order_user,
+                        pharmacy: order_pharmacy,
+                        products: order_products,
+                        price: order_price,
+                        location: order_location,
+                        time: order_time,
+                        status: order_status,
+                        imageUrl: image_url
+
+
+                 };
+
+
+                                db.collection("orders").insertOne(order, function(err, res) {
+                                        if (err){
+                                        }else{
+                                                if(order_image!==null || order_image!==""){
+
+                                                    var req = http.request(function(res) {
+                                                      var file = fs.createWriteStream(order_image);
+                                                      res.pipe(file);
+                                                    });
+                                                    req.on('error', function(e) {
+                                                      console.log('error: ' + e.message);
+                                                    });
+                                                    req.end();
+                                                }
+                                                
+                                        }
+                
+                                        db.close();
+                                });
+                        
+
+
+
+});
+ 
+
+})
+
+app.get('/order',staticUserAuth,function(req,res){
+    //res.redirect("/order")
+})
 
 app.listen(3000, function() {
     console.log("Listening!")
